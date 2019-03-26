c1b3ad25493ab5741b718e46ccd533003042c205
Jean Valderrama <jeanvalderrama@Jeans-MacBook-Pro.local>
2019-03-26 09:16:34 -0400

    almost done with phase 02, working in doWhatItSays

--- liri.js
+++ liri.js
@@ -62,28 +62,38 @@ function spotifyThisSong() {
     if (commandInfo === undefined) {
         commandInfo = "The+Sign+Ace+of+Base";
     }
+    // response data retunrs one song    
+    spotify.search({ type: 'track', query: commandInfo, limit: 1 })
+        .then(function (response) {
+            //   console.log(response);
+            //   console.log(response.tracks.items[0]);
+            var artists = ("Artists: " + response.tracks.items[0].album.artists[0].name);
+            // console.log(artists);
+            var songName = ("Song: " + response.tracks.items[0].name);
+            // console.log(songName);
+            var previewLink = ("Preview link: " + response.tracks.items[0].preview_url);
+            // console.log(previewLink);
+            var albumName = ("Album: " + response.tracks.items[0].album.name);
+            // console.log(albumName);
 
-    spotify.search(
-        { 
-            type: 'track', 
-            query: commandInfo 
-        },
-        function(err,response) {
-            if(err){
-                console.log(err);
-                return;
-            }
-            //array of songs that we get back
-            var songs = response.tracks.items;
-            //loop over the array of songs 
-            for (let index = 0; index < songs.length; index++) {
-                //log out the artists
-                 console.log(songs[index].artists);
-                
-            }
-        });
-        
-        
+            // append to log.txt
+            fs.appendFile("log.txt", artists + "\n" + songName + "\n" + previewLink + "\n" + albumName + "\n" + "" + "\n", function (error) {
+                // Checks to see if theres an error appending to the file
+                if (error) {
+                    console.log(error);
+                } else {
+                    console.log("Text is added to the log.txt");
+                }
+            })
+
+
+
+
+
+        })
+
+
+
 }
 
 
@@ -98,7 +108,32 @@ function concertThis() {
 
     axios.get(queryURL).then(
         function (response) {
-            console.log(response);
+            // console.log((response));
+            // console.log(response.data[i].venue);
+            // for loop for the venues
+            for (let i = 0; i < response.data.length; i++) {
+                var venueName = "Venue name: " + response.data[i].venue.name;
+                // console.log(venueName);
+
+                var venueLocation = "Venue location: " + response.data[i].venue.city + " , " + response.data[i].venue.region
+                // console.log(venueLocation);
+
+                var venueDate = "Venue Date: " + (response.data[i].datetime); // check the format for moment .format("L")
+                // console.log(venueDate);
+
+                // append to log.txt
+                fs.appendFile("log.txt", venueName + "\n" + venueLocation + "\n" + venueDate + "\n" + "" + "\n", function (error) {
+                    // Checks to see if theres an error appending to the file
+                    if (error) {
+                        console.log(error);
+                    } else {
+                        console.log("Text is added to the log.txt");
+                    }
+                })
+
+
+
+            }
 
         }
     );
@@ -108,46 +143,61 @@ function concertThis() {
 
 function movieThis() {
     // this is no movie is provided then the program by default will render "Mr. Nobody"
-   if (commandInfo === undefined) {
-       commandInfo = "Mr. +Nobody";
-   }
+    if (commandInfo === undefined) {
+        commandInfo = "Mr. +Nobody";
+    }
 
-   var queryUrl = "http://www.omdbapi.com/?t=" + commandInfo + "&y=&plot=short&apikey=trilogy";
+    var queryUrl = "http://www.omdbapi.com/?t=" + commandInfo + "&y=&plot=short&apikey=trilogy";
 
-   axios.get(queryUrl).then(
-       function (response) {
-           //   console.log(response)
+    axios.get(queryUrl).then(
+        function (response) {
+            //   console.log(response)
 
-           var title = "Title" + response.data.Title;
-           // console.log(title);
-           var year = "year" + response.data.Year;
-           // console.log(year);
-           var imdbRatingResult = "IMDB Rating" + response.data.imdbRating;
-           // console.log(imdbRatingResult);
-           var rottenTomatoesRating = " Rotten Tomatoes Rating" + response.data.Ratings[1].value;
-           //console.log(rottenTomatoesRating);
-           var country = "Country" + response.data.Country;
-           // console.log(country);
-           var language = "language" + response.data.Language;
-           // console.log(language);
-           var plot = "Plot" + response.data.Plot;
-           // console.log(plot);
-           var actors = "Actors" + response.data.Actors;
-           // console.log(actors);
+            var title = "Title" + response.data.Title;
+            // console.log(title);
+            var year = "year" + response.data.Year;
+            // console.log(year);
+            var imdbRatingResult = "IMDB Rating" + response.data.imdbRating;
+            // console.log(imdbRatingResult);
+            var rottenTomatoesRating = " Rotten Tomatoes Rating" + response.data.Ratings[1].value;
+            //console.log(rottenTomatoesRating);
+            var country = "Country" + response.data.Country;
+            // console.log(country);
+            var language = "language" + response.data.Language;
+            // console.log(language);
+            var plot = "Plot" + response.data.Plot;
+            // console.log(plot);
+            var actors = "Actors" + response.data.Actors;
+            // console.log(actors);
 
 
-           // append to log.txt
-           fs.appendFile("log.txt", title + "\n" + year + "\n" + imdbRatingResult + "\n" + rottenTomatoesRating + "\n" + language + "\n"  + country + "\n" + plot + "\n" + actors + "\n" + "" + "\n", function (error) {
-               // Checkes to see if theres an error appending to the file
-               if (error) {
-                   console.log(error);
-               } else {
-                   console.log("Text is added to the log.txt");
-               }
-           })
-       })
+            // append to log.txt
+            fs.appendFile("log.txt", title + "\n" + year + "\n" + imdbRatingResult + "\n" + rottenTomatoesRating + "\n" + language + "\n" + country + "\n" + plot + "\n" + actors + "\n" + "" + "\n", function (error) {
+                // Checkes to see if theres an error appending to the file
+                if (error) {
+                    console.log(error);
+                } else {
+                    console.log("Text is added to the log.txt");
+                }
+            })
+        })
 }
-start();
+
+    // function doWhatItSays 
+    // Running the readFile module that's inside of fs.
+    // Stores the read information into the variable "data"
+    // if error if found it will console log err
+    function doWhatItSays(){  
+    fs.readFile("random.txt", "utf8", function(err, data) {
+        if (error) {
+        return console.log(error);
+        }
+        // reassign values and variables to text in random.txt file
+         // Break the string down by comma separation and store the contents into the output array.
+        var output = data.split(",");
+
+    }
+    // start();
 
 
 
